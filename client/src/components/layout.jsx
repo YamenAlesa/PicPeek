b77630a4a09bc607551be392191d46a46f04054e
Yamen <yamenx2005@gmail.com>
2025-01-29 10:28:07 +0100

    Remove Footer component import from layout

--- client/src/components/layout.jsx
+++ client/src/components/layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router";
 import Navbar from "./navbar";
-import Footer from "./footer"; 
+//import Footer from "./footer"; 
 
 const Layout = () => {
   return (
